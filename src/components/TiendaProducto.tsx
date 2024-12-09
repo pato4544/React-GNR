70d0f06e868cd2ef8595a843174c1d832509c775
pato4544 <patricioperezjudo@gmail.com>
2024-12-09 13:24:23 -0300

    Src

---
+++ src/components/TiendaProducto.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TiendaProducto = () => {
+  return (
+    <div className='min-h-screen bg-black'>
+        
+        
+    </div>
+  )
+}
+
+export default TiendaProducto
