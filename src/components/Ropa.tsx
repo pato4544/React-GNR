b68482155086931f4070bbfd02bf6069fa439162
pato4544 <patricioperezjudo@gmail.com>
2024-12-09 10:46:07 -0300

    Delete src directory

--- src/components/Ropa.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Ropa = () => {
-  return (
-    <div>Ropa</div>
-  )
-}
-
-export default Ropa
--- src/type/ITimeline.ts
+++
@@ -1,5 +0,0 @@
-export default interface ITimeline {
-    titulo: string;
-    contenido: string;
-    imagen: string;
-}
