1343372ce3440b9d7fdf75573bba9e1203a6e4ee
pato4544 <patricioperezjudo@gmail.com>
2024-12-13 11:18:37 -0300

    Delete src directory

--- src/type/INoticias.ts
+++
@@ -1,6 +0,0 @@
-export interface INoticias { // Es una pesima practica exportar las interfaces con el default porque despues las tenes que importar manualmente, no te da el atajo
-    imagenNoticia: string;
-    titulo: string;
-    fecha: string;
-    contenido: string;
-}
