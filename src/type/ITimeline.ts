e217e9dbf9b385c97592bdf627de24e9906a0f74
pato4544 <patricioperezjudo@gmail.com>
2025-01-16 15:09:09 -0300

    Version 1.2 FULL

--- src/type/ITimeline.ts
+++
@@ -1,6 +0,0 @@
-export interface ITimeline { // Es una pesima practica exportar las interfaces con el default porque despues las tenes que importar manualmente, no te da el atajo
-    id: number;
-    titulo: string;
-    contenido: string;
-    imagen: string;
-}
